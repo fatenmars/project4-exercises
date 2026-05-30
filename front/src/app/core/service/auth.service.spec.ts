0f93fb04922f20cefb46a310730bb27256e63a09
Faten <faten.mars@mars-tech.dev>
2026-05-30 02:00:06 +0200

    WIP AuthService tests

---
+++ front/src/app/core/service/auth.service.spec.ts
@@ -0,0 +1,29 @@
+import { AuthService } from './auth.service';
+import { TestBed } from '@angular/core/testing';
+import {
+  provideHttpClientTesting,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { describe, it, expect, beforeEach, afterEach } from '@jest/globals';
+
+describe('AuthService', () => {
+  let service: AuthService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    });
+    httpMock = TestBed.inject(HttpTestingController);
+    service = TestBed.inject(AuthService);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('devrait être créé', () => {
+    expect(service).toBeTruthy();
+  });
+});
