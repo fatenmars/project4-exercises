b65fdbe7ac400bb4775874bdaec9524856505c5f
Faten <faten.mars@mars-tech.dev>
2026-05-31 01:23:00 +0200

    DONE TeacherService tests

--- front/src/app/core/service/teacher.service.spec.ts
+++ front/src/app/core/service/teacher.service.spec.ts
@@ -1,22 +1,73 @@
-import { HttpClientModule } from '@angular/common/http';
+import {
+  provideHttpClientTesting,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
-
+import { provideHttpClient } from '@angular/common/http';
 import { TeacherService } from './teacher.service';
 
 describe('TeacherService', () => {
   let service: TeacherService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports:[
-        HttpClientModule
-      ]
+      providers: [provideHttpClient(), provideHttpClientTesting()],
     });
+    httpMock = TestBed.inject(HttpTestingController);
     service = TestBed.inject(TeacherService);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get all teachers', () => {
+    const mockTeachers = [
+      {
+        id: 1,
+        lastName: 'Doe',
+        firstName: 'John',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: 2,
+        lastName: 'Smith',
+        firstName: 'Jane',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ];
+    service.all().subscribe((teachers) => {
+      expect(teachers).toEqual(mockTeachers);
+    });
+
+    const req = httpMock.expectOne('api/teacher');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockTeachers);
+  });
+
+  it('should get teacher detail', () => {
+    const mockTeacher = {
+      id: 1,
+      lastName: 'Doe',
+      firstName: 'John',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    service.detail('1').subscribe((teacher) => {
+      expect(teacher).toEqual(mockTeacher);
+    });
+
+    const req = httpMock.expectOne('api/teacher/1');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockTeacher);
   });
 });
